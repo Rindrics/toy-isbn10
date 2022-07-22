6e7401f82fdc4ef3ee89504f6e8d39c6fa836898
Akira Hayashi <smxshxishxad@me.com>
2022-07-23 06:15:58 +0900

    :construction: Move `validate_isbn10` under class

--- isbncheck.py
+++ isbncheck.py
@@ -1,20 +1,24 @@
-def validate_isbn10(isbn):
-    if len(isbn) != 10:
-        return "invalid"
+class ISBN10:
+    def __init__(self):
+        pass
 
-    isbn = [int(i) for i in isbn if i != 'x' and i != 'X']
+    def validate_isbn10(self, isbn):
+        if len(isbn) != 10:
+            return "invalid"
 
-    if len(isbn) == 9:  # this occurs when 'x' is omitted by previous procedure
-        isbn.append(10)
+        isbn = [int(i) for i in isbn if i != 'x' and i != 'X']
 
-    total = sum([(10 - i) * isbn[i] for i in range(len(isbn))])
+        if len(isbn) == 9:  # this occurs when 'x' is omitted by previous procedure
+            isbn.append(10)
 
-    if total % 11 == 0:
-        return "valid"
-    else:
-        return "invalid"
+        total = sum([(10 - i) * isbn[i] for i in range(len(isbn))])
+
+        if total % 11 == 0:
+            return "valid"
+        else:
+            return "invalid"
 
 
 if __name__ == "__main__":
     isbn = "123456789x"
-    print("'{}' is {} ISBN-10 number".format(isbn, validate_isbn10(isbn)))
+    print("'{}' is {} ISBN-10 number".format(isbn, ISBN10().validate_isbn10(isbn)))
--- isbncheck_test.py
+++ isbncheck_test.py
@@ -1,12 +1,12 @@
 import pytest
-from isbncheck import validate_isbn10
+from isbncheck import ISBN10
 
 
-def test_validate_isbn10():
-    assert validate_isbn10("123456789x") == "valid"
-    assert validate_isbn10("1234567891") == "invalid"
+def test_ISBN10():
+    assert ISBN10().validate_isbn10("123456789x") == "valid"
+    assert ISBN10().validate_isbn10("1234567891") == "invalid"
 
 
 def test_invalid_length():
-    assert validate_isbn10("too-short") == "invalid"
-    assert validate_isbn10("to-loooooooooong") == "invalid"
+    assert ISBN10().validate_isbn10("too-short") == "invalid"
+    assert ISBN10().validate_isbn10("to-loooooooooong") == "invalid"
